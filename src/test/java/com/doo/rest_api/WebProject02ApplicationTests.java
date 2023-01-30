b288d56dfed3a735a9706cc8d862518ca1019311
Hayden-Doo <heton.doo@gmail.com>
2023-01-30 18:13:22 +0900

    add Object Mapper

--- src/test/java/com/doo/rest_api/WebProject02ApplicationTests.java
+++ src/test/java/com/doo/rest_api/WebProject02ApplicationTests.java
@@ -1,5 +1,8 @@
 package com.doo.rest_api;
 
+import com.doo.rest_api.dto.User02;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 class WebProject02ApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() throws JsonProcessingException {
+		System.out.println("-------------------------");
 
+		// Text JSON -> Object
+		// Object -> Text JSON
+
+		// controller req json(text) -> object
+		// response object -> json(text)
+
+		var objectMapper = new ObjectMapper();
+
+		// object -> text
+		// "object mapper get method" using...
+		var user02 = new User02("doo", 10, "010-1111-222");
+		var text = objectMapper.writeValueAsString(user02);
+		System.out.println(text);
+
+		// text -> object
+		// "object mapper" need default constructor
+		var objectUser02 = objectMapper.readValue(text, User02.class);
+		System.out.println(objectUser02);
+
+	}
 }
